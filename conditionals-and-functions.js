ca813163abed4b7786cdd839cc1448e484bf70fc
Mike Athey <mathey@cnm.edu>
2024-04-04 10:56:26 -0600

    finishing conditionals and functions

--- conditionals-and-functions.js
+++ conditionals-and-functions.js
@@ -89,9 +89,21 @@ function addNumbers (n, m, x) {
 let ourArray = [1, 2, 3, 4, 5, 6, 7];
 
 //loops
-
+/*
 let arrLen = ourArray.length;
 for(let i = 0; i<arrLen; i++){
     //console.log("is equal to: " + i);
    console.log(ourArray[i]);
+   for (let j = 0; j<10; j++) {
+       console.log('j is equal to: ' + j);
+   }
 }
+*/
+
+/*
+let x = 0
+while (x < 10) {
+    console.log('Ran');
+    x = x + 1;
+}
+ */
